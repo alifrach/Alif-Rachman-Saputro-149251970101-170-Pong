0b9b142775e4be26f1b61eb9c6e7080e09023705
alifrach <arachsapwk@gmail.com>
2022-06-10 17:00:02 +0700

    Adding UI & Game Manager

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public Vector2 speed;
+    public Vector2 speed, resetPosition;
     private Rigidbody2D rig;
     // Start is called before the first frame update
     private void Start()
@@ -18,4 +18,8 @@ public class BallController : MonoBehaviour
     {
     }
 
+    public void ResetBall() 
+    { 
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2); 
+    } 
 }
